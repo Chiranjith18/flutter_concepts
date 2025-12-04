94789d8600404c10387d195d5f0b806e835bcd53
Chiranjith18 <145703565+Chiranjith18@users.noreply.github.com>
2025-12-04 10:09:40 +0530

    Create Stateful widget with setstate.dart

---
+++ Stateful widget with setstate.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor:Colors.teal,brightness: Brightness.dark)),
+      home:MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget{
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+
+
+}
+
+class _MyHomePageState extends State<MyHomePage>{
+
+  int currentindex=0;
+  @override
+Widget build(BuildContext context) {
+  return Scaffold(
+  appBar:AppBar(
+title: Text('Fluteer'),
+centerTitle: true,
+),
+
+body:currentindex==0?Center(child:Text('1'
+)):Center(child:Text('2'
+)),
+      bottomNavigationBar: NavigationBar(destinations: [
+        NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+        NavigationDestination(icon: Icon(Icons.person), label: 'Profile')
+      ],
+        onDestinationSelected: (int value){
+
+         setState(() {
+           currentindex = value;
+         });
+        },
+        selectedIndex: currentindex,
+      ),
+    );
+  }
+
+}
